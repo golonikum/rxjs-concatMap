c8a674e14c4dad14440c847de3fb52c61d48d79c
golonikum <goloniko@gmail.com>
2019-10-18 13:26:54 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { of } from 'rxjs';
 import { concatMap, switchMap, delay, mergeMap } from 'rxjs/operators';
 
+console.log(`------------------- concatMap ---------------------`)
+
 //emit delay value
 const source = of(1000, 500, 2000, 800);
 // map value from source into inner observable, when complete emit result and move to next
@@ -12,6 +14,7 @@ const subscribe = example.subscribe(val =>
   console.log(`With concatMap: ${val}`)
 );
 
+console.log(`------------------- mergeMap ---------------------`)
 // showing the difference between concatMap and mergeMap
 const mergeMapExample = source
   .pipe(
@@ -22,6 +25,7 @@ const mergeMapExample = source
   .subscribe(val => console.log(`With mergeMap: ${val}`));
 
 
+console.log(`------------------- switchMap ---------------------`)
 // showing the difference between concatMap and mergeMap
 const switchMapExample = source
   .pipe(
